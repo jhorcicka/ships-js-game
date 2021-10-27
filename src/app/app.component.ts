f98fed5688b4a9182d838ac901658e8dc26ccc26
pk46 <kupcik46@gmail.com>
2021-10-27 19:12:45 +0200

    5: trida Cell

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,15 +8,32 @@ import { Cell } from './domain/cell'
 })
 export class AppComponent {
   title = 'ships';
+  userCells: Cell[] = []
+  aiCells: Cell[] = []
   CELL_COUNT = 100
-  getCells(prefix:string){
-    let cells = []
+  createCells(prefix:string): Cell[]{
+    let cells: Cell[] = []
     for (let i = 0; i<this.CELL_COUNT;i++) {
-      cells.push({'id':prefix+"_"+i})
+      let cell = new Cell(i, prefix)
+      console.log(cell.toString())
+      cells.push(cell)
     }
+
     return cells
   }
   onCellClick(cellId:string) {
-  console.log(cellId)
+    console.log(cellId)
+  }
+  getUserCells(): Cell[] {
+    if (this.userCells.length == 0) {
+      this.userCells = this.createCells("user")
+    }
+    return this.userCells
+  }
+  getAICells(): Cell[] {
+    if (this.aiCells.length == 0) {
+      this.aiCells = this.createCells("ai")
+    }
+    return this.aiCells
   }
 }
--- src/app/domain/cell.ts
+++ src/app/domain/cell.ts
@@ -1,15 +1,28 @@
 import { Status } from './status';
 
 export class Cell {
+  index: number
   x: number
   y: number
   status: Status
+  idPrefix: string
 
-  constructor(x: number, y: number, status: Status){
-    this.x = x
-    this.y = y
-    this.status = status
+  constructor(index: number, idPrefix: string){
+    this.index = index
+    this.x = 0
+    this.y = 0
+    this.status = Status.EMPTY
+    this.idPrefix = idPrefix
   }
   onClick(event:object): void {}
   draw(): void {}
+  getIndex(): number {
+    return this.index
+    }
+   getIndexAsString(): string {
+    return this.idPrefix+"_"+this.index
+   }
+   toString(): string {
+    return "Cell[idPrefix ="+this.idPrefix+", index="+this.index+",status="+this.status+"]"
+   }
 }
